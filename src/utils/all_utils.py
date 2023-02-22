3ef84c3918c6e65fc9dbdbd4df473c4ecd8dba3c
Prakhar2295 <prakhar.kumar5@gmail.com>
2023-02-22 06:04:06 +0530

    all_utills -decodeimage added

--- src/utils/all_utils.py
+++ src/utils/all_utils.py
@@ -3,6 +3,9 @@ import os
 import time
 import json
 import logging
+import base64
+
+
 
 
 def read_yaml(path_to_yaml: str) -> dict:
@@ -21,30 +24,15 @@ def get_timestamp(name):
     unique_name = f"{name}_at_{timestamp}"
     return unique_name
 
-import re
-import base64
 
-import numpy as np
-
-from PIL import Image
-from io import BytesIO
-
-
-def base64_to_pil(img_base64):
-    """
-    Convert base64 image data to PIL image
-    """
-    image_data = re.sub('^data:image/.+;base64,', '', img_base64)
-    pil_image = Image.open(BytesIO(base64.b64decode(image_data)))
-    return pil_image
+def decodeImage(imgstring, fileName):
+    imgdata = base64.b64decode(imgstring)
+    with open(fileName, 'wb') as f:
+        f.write(imgdata)
+        f.close()
 
 
-def np_to_base64(img_np):
-    """
-    Convert numpy image (RGB) to base64 string
-    """
-    img = Image.fromarray(img_np.astype('uint8'), 'RGB')
-    buffered = BytesIO()
-    img.save(buffered, format="PNG")
-    return u"data:image/png;base64," + base64.b64encode(buffered.getvalue()).decode("ascii")    
+def encodeImageIntoBase64(croppedImagePath):
+    with open(croppedImagePath, "rb") as f:
+        return base64.b64encode(f.read())    
        
